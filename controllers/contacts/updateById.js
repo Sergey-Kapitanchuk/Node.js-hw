4b7e8a36e85dcf2b1f0e6beba759e74240c4e4f8
Sergey-Kapitanchuk <kapitanchukserg28@gmail.com>
2022-10-16 21:49:52 +0300

    i

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -4,6 +4,9 @@ const { RequestError } = require("../../helpers");
 
 const updateById = async (req, res) => {
     const { contactId } = req.params;
+    if (!req.body) {
+        throw RequestError(400, 'missing field favorite')
+    }
     const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
     if (!result) {
         throw RequestError(404, 'Not found')
