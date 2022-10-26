d3f453eb4f3005a014070e466c534c40984f7b0a
Sergey-Kapitanchuk <kapitanchukserg28@gmail.com>
2022-10-26 04:16:38 +0300

    fixes

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,7 +4,7 @@ const getAll = async (req, res) => {
     const { _id: owner } = req.user;
     const { page = 1, limit = 5 } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit });
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email");
     res.json(result)
 };
 
