ee575b1c53959c66ff74c5a7cdadd2b960a67e7c
Sergey-Kapitanchuk <kapitanchukserg28@gmail.com>
2022-10-26 04:11:19 +0300

    на перевірку

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,8 +1,10 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-    const result = await Contact.find({}, "-createdAt -updatedAt");
-    console.log(result);
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 5 } = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit });
     res.json(result)
 };
 
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,13 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
-const validFavoriteBody = require("./validFavoriteBody")
+const validFavoriteBody = require("./validFavoriteBody");
+const validateUser = require("./validateUser");
+const authenticate = require("./authenticate");
 
 module.exports = {
     validateBody,
     isValidId,
     validFavoriteBody,
+    validateUser,
+    authenticate,
 }
